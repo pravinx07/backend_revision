796004f9eb64b71895d221ba68128573081fc3f1
MrPravinS <shegamwarpravin07@gmail.com>
2025-10-20 19:30:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+
+const app = express()
+
+// to access .env variables
+dotenv.config()
+
+const PORT = process.env.PORT || 4000
+
+
+app.get("/", (req,res) => {
+    res.send("Hey there")
+})
+
+app.get("/twitter", (req,res) => {
+    res.send("pravin_07")
+})
+
+app.get("/login", (req,res) => {
+    res.send("<h1>Please login at DevHub</h1>")
+})
+app.get("/youtube", (req,res) => {
+    res.send("<p>Please go to YoutTube</p>")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+    
+})
